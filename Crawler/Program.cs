80c3869874623dc3ff158e468de1c23a6005c02e
MorFixLaptop <a@b.c>
2017-09-10 09:12:42 +0300

    added Mock "Main"

---
+++ Crawler/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace Crawlers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Op().Wait();
+        }
+
+        private static async Task Op()
+        {
+            await Task.FromResult(true);
+        }
+    }
+}
