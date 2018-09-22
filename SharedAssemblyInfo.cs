903a185eb59c36cf3ac7b731c3cb9b8edc9bfd4b
MorFixLaptop <yapan7@outlook.com>
2018-09-23 02:17:05 +0300

    fix(build): fixing build issues
    * creating publish config
    * updating version to 3.5
    * removing pre-set values in settings.config

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -4,9 +4,14 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Mor Cohen")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -23,5 +28,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.3.0.0")]
-[assembly: AssemblyFileVersion("3.3.0.0")]
+[assembly: AssemblyVersion("3.5.0.0")]
+[assembly: AssemblyFileVersion("3.5.0.0")]
